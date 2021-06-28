5947ac4f90fdd2dc6fae76a6109639dcff314073
Alex Zhou <alexzhou90@gmail.com>
2021-06-28 15:22:43 -0400

    changed sender.py

--- testing/sender.py
+++ testing/sender.py
@@ -1,4 +1,6 @@
 import cereal.messaging as messaging
 
-sm = messaging.SubMaster(['carState'])
-
+pm = messaging.PubMaster(['sensorEvents'])
+dat = messaging.new_message('sensorEvents', size=1)
+dat.sensorEvents[0] = {"gyro": {"v": [0.1, -0.1, 0.1]}}
+pm.send('sensorEvents', dat)
