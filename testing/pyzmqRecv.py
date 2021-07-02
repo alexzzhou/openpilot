f74befd95f02f30252fd84d2303d80886509671f
Alex Zhou <alexzhou90@gmail.com>
2021-07-02 12:06:21 -0400

    changed receiver again

--- testing/pyzmqRecv.py
+++ testing/pyzmqRecv.py
@@ -7,7 +7,7 @@ socket = context.socket(zmq.SUB)
 socket.connect("tcp://localhost:5556")
 
 
-socket.setsockopt_string(zmq.SUBSCRIBE, "1")
+socket.setsockopt_string(zmq.SUBSCRIBE, "")
 
 # Process 5 updates
 
