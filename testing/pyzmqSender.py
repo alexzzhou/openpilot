92ee25f5fc1fc75fd22e13a06d6970f07c81dc4a
Alex Zhou <alexzhou90@gmail.com>
2021-07-02 12:22:55 -0400

    updated sender test

--- testing/pyzmqSender.py
+++ testing/pyzmqSender.py
@@ -6,11 +6,11 @@ sm = messaging.SubMaster(['carState'])
 
 context = zmq.Context()
 socket = context.socket(zmq.PUB)
-socket.bind("tcp://127.0.0.1:5556")
+socket.bind("tcp://10.34.34.26:5556")
 
 while True:
     sm.update(0)
     state = sm['carState']
-    
+    print(state)
     socket.send_string(str(state.vEgo))
     time.sleep(1)
