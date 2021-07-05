b0e3314ed185f68f144d301dc762dd07db0f6380
Alex Zhou <alexzhou90@gmail.com>
2021-07-05 10:27:53 -0400

    testing using capnp

--- testing/pyzmqRecv.py
+++ testing/pyzmqRecv.py
@@ -1,5 +1,7 @@
 import time
 import zmq
+import capnp
+from cereal import log
 
 #  Socket to talk to server
 context = zmq.Context()
@@ -7,9 +9,9 @@ socket = context.socket(zmq.SUB)
 socket.connect("tcp://10.34.34.104:9000")
 
 
-socket.setsockopt_string(zmq.SUBSCRIBE, "")
+socket.setsockopt(zmq.SUBSCRIBE, "")
 
 
 while True:
-    string = socket.recv_string()
-    print(string)
+    data = socket.recv()
+    
