601148683ea00907d1d821e0f125f17a73ea5cdd
Alex Zhou <alexzhou90@gmail.com>
2021-07-08 10:51:08 -0400

    testing

--- testing/info.py
+++ testing/info.py
@@ -1,10 +1,10 @@
 import time
 import cereal.messaging as messaging
 
-sm = messaging.SubMaster(['sendcan', 'controlsState', 'carState','carControl', 'carEvents', 'carParams'])
+sm = messaging.SubMaster(['sendcan', 'controlsState', 'carState','carControl', 'carEvents', 'carParams', 'driverCameraState'])
 
 while True:
     sm.update(0)
-    state = sm['carState']
+    state = sm['driverCameraState']
     print(state)
     time.sleep(1)
